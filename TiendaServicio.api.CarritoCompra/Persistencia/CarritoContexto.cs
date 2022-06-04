f8b253da0035d1e2d5f5b8b906f37da4e0a09b69
erick castro <erickpaulcm27@gmail.com>
2022-06-04 01:12:37 -0700

    -- Microservice CarritoCompra pt1

---
+++ TiendaServicio.api.CarritoCompra/Persistencia/CarritoContexto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using TiendaServicio.api.CarritoCompra.Modelo;
+
+namespace TiendaServicio.api.CarritoCompra.Persistencia
+{
+    public class CarritoContexto :DbContext
+    {
+        public CarritoContexto(DbContextOptions<CarritoContexto> options):base(options){}
+        public DbSet<CarritoSesion> CarritoSesion { get; set; }
+        public DbSet<CarritoSesionDetalle> CarritoSesionDetalle { get; set; }
+    }
+}
--- TiendaServicio.api.CarritoCompra/Startup.cs
+++ TiendaServicio.api.CarritoCompra/Startup.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TiendaServicio.api.CarritoCompra.Persistencia;
 
 namespace TiendaServicio.api.CarritoCompra
 {
